532192cac0e1c9777811977dba79825e0699fd38
kpcomcast <>
2021-05-25 02:04:12 +0000

    assignment 3, submission 1

--- rover.js
+++ rover.js
@@ -1,5 +1,47 @@
 class Rover {
-   // Write code here!
+  constructor(position) {
+    this.position = position;
+    this.mode = "NORMAL";
+    this.generatorWatts = 110;
+  }
+
+  receiveMessage(message) {
+    let resp = {
+      message: message.name,
+      results: []
+    }
+    let arr = message.commands;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].commandType === "STATUS_CHECK") {
+        arr[i] = {
+          completed: true,
+          roverStatus: {
+            mode: this.mode,
+            generatorWatts: this.generatorWatts,
+            position: this.position
+          }
+        }
+      } else if (arr[i].commandType === "MODE_CHANGE") {
+        this.mode = arr[i].value;
+        arr[i] = {
+          completed: true
+        };
+      } else if (arr[i].commandType === "MOVE") {
+        if (this.mode === "LOW_POWER") {
+          arr[i] = {
+            completed: false
+          }
+        } else if (this.mode === "NORMAL") {
+          this.position = arr[i].value;
+          arr[i] = {
+            completed: true
+          }
+        }
+      }
+    }
+    resp.results = arr;
+    return resp;
+  }
 }
 
 module.exports = Rover;
